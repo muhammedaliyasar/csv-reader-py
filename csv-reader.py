7fd25c6ee260122f714e4334a7b1e07e696f504b
Muhammed Ali Yasar <muhammedyasar@eksi16.com>
2022-03-15 23:50:42 +0300

    read csv file and show csv file content function

--- csv-reader.py
+++ csv-reader.py
@@ -1,3 +1,14 @@
 import pandas as pd
-import numpy as np
+
+class CsvReader():
+    """
+        Excel reader
+    """
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.data_frame = pd.read_csv(f"{file_name}")
+
+    def showDataFrame(self):
+        print(self.data_frame)
+
 
